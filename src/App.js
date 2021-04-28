647a61a237cb7474472bb9719cdb797b2de02411
Gamoundo <akilgg@gmail.com>
2021-04-28 19:33:38 -0400

    started the navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Chat from './Chat';
+import Navi from './Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+      <Navi />
       <Chat />
     </div>
   );
---
+++ src/Navbar.js
@@ -0,0 +1,11 @@
+import Navbar from 'react-bootstrap/Navbar'
+
+function Navi() {
+    return(
+        <div>
+            <h1>hi</h1>
+        </div>
+    )
+}
+
+export default Navi;
