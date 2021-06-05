02d88e8bd5b6209c251e17e02baab268c591c278
Gamoundo <akilgg@gmail.com>
2021-06-05 11:04:06 -0400

    added followers to the page

--- src/User.js
+++ src/User.js
@@ -3,13 +3,23 @@ import React  from "react";
 class User extends React.Component {
 
     state = {
-        userName: "Bob"
+        userName: "Bob",
+        followers: 0,
+        profilePic: "https://www.tynker.com/projects/screenshot/575088a4588161fe118b4573/derp-face.png"
     }
 
     render () {
         return(
-            <div>
-                <h1> About {this.state.userName}</h1>
+            <div className="info">
+                <div>
+                  <img className="image" src={this.state.profilePic} alt="face"/>
+                  <p> {this.state.followers} followers</p>
+                </div>
+                <div>
+                   <h1>{this.state.userName}</h1>
+                </div>
+                
+
             </div>
         )
     }
