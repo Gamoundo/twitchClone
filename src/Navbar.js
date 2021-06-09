4f9d819c47de266bfb0e6fc174375206c2d277de
Gamoundo <akilgg@gmail.com>
2021-06-09 16:19:36 -0400

    added a navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,43 @@
-import Navbar from 'react-bootstrap/Navbar'
+import React, { useState } from 'react';
+import {
+  MDBContainer,
+  MDBNavbar,
+  MDBNavbarBrand,
+  MDBNavbarToggler,
+  MDBNavbarNav,
+  MDBNavbarItem,
+  MDBNavbarLink,
+  MDBIcon,
+  MDBCollapse
+} from 'mdb-react-ui-kit';
 
-function Navi() {
-    return(
-        <div className="nav">
-            <h1>hi</h1>
-        </div>
-    )
-}
+export default function Navi() {
+  const [showNavSecond, setShowNavSecond] = useState(false);
 
-export default Navi;
+  return (
+    <MDBNavbar expand='lg' light bgColor='light'>
+      <MDBContainer fluid>
+        <MDBNavbarBrand href='#'>Navbar</MDBNavbarBrand>
+        <MDBNavbarToggler
+          aria-expanded='false'
+          aria-label='Toggle navigation'
+          onClick={() => setShowNavSecond(!showNavSecond)}
+        >
+          <MDBIcon icon='bars' fas />
+        </MDBNavbarToggler>
+        <MDBCollapse navbar show={showNavSecond}>
+          <MDBNavbarNav>
+            <MDBNavbarLink active aria-current='page' href='#'>
+              Following
+            </MDBNavbarLink>
+            <MDBNavbarLink href='#'>Browse</MDBNavbarLink>
+            <MDBNavbarLink href='#'>Esports</MDBNavbarLink>
+            <MDBNavbarLink disabled href='#' tabIndex={-1} aria-disabled='true'>
+              Music
+            </MDBNavbarLink>
+          </MDBNavbarNav>
+        </MDBCollapse>
+      </MDBContainer>
+    </MDBNavbar>
+  );
+  }
